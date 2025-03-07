b7c608abb66cb74ac3f8d06a0873309928a716fc
Dangtae <youngtae0818@naver.com>
2025-03-07 17:17:49 +0900

    ✨ Solve the problem 1026

--- youngtae/greedy/1026.py
+++ youngtae/greedy/1026.py
@@ -9,11 +9,11 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-A.sort(reverse= True)
-B.sort()
+A.sort(reverse= True) # A는 내림차순 정렬
+B.sort()              # B는 오름차순 정렬
 
 answer = 0
-for x, y in zip(A,B):
+for x, y in zip(A,B): # 정렬된 A,B 각각 곱하기
     answer += x * y
-    
+
 print(answer)
